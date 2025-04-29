add245d56c7a520175352438b40e712e3303fc84
Matthew <matthewjwright07@gmail.com>
2025-04-29 12:21:53 -0500

    got rid of speed analytics

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import Header from "@/components/Layout/Header";
 import "./globals.css";
 import Footer from "@/components/Layout/Footer";
 import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   title: "Webity",
@@ -18,7 +17,6 @@ export default function RootLayout({ children }) {
         {children}
         <Footer/>
         <Analytics/>
-        <SpeedInsights/>
       </body>
     </html>
   );
