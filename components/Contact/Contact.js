c0a872959f02f89d795f914eb13fad7ebdc3077d
Matthew <matthewjwright07@gmail.com>
2025-06-26 13:29:58 -0500

    updates

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -37,22 +37,22 @@ export default function Contact(){
 
     return (
         <div className="px-8 lg:px-32 py-12 flex flex-col justify-center items-center gap-4">
-            <div className="flex flex-col justify-center w-full border border-white rounded-lg p-10 gap-4">
+            <div className="flex flex-col justify-center w-full lg:w-3/4 border border-white rounded-lg p-10 gap-4">
                 <h1 className="text-2xl font-bold text-center">Contact</h1>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="to">To</label>
-                    <select name="product" id="product" className="border border-white rounded-lg p-3 hover:cursor-pointer">
+                    <select name="to" id="to" className="border border-white rounded-lg p-3 hover:cursor-pointer">
                         <option value="webdev" className="text-white bg-black">info@webity.io</option>
                         <option value="uiux" className="text-white bg-black">512-586-4786</option>
                     </select>
                 </div>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="name">Name</label>
-                    <input onChange={handleName} value={name} id="name" className="rounded-lg p-2 border border-white "/>
+                    <input onChange={handleName} value={name} id="name" className="rounded-lg p-3 border border-white "/>
                 </div>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="email">Email</label>
-                    <input onChange={handleEmail} value={email} id="email" className="rounded-lg p-2 border border-white "/>
+                    <input onChange={handleEmail} value={email} id="email" className="rounded-lg p-3 border border-white "/>
                 </div>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="product">Product</label>
@@ -67,7 +67,7 @@ export default function Contact(){
                 </div>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="message">Message</label>
-                    <textarea onChange={handleMessage} value={message} id="message" className="rounded-lg p-2 border border-white "/>
+                    <textarea onChange={handleMessage} value={message} id="message" className="rounded-lg p-3 border border-white "/>
                 </div>
                 <div className="flex justify-center">
                     <button onClick={handleClick} className="w-full h-12 bg-primary rounded-lg flex items-center justify-center hover:bg-secondary transition duration-500 hover:cursor-pointer">Submit</button>
