8ddb1bcf0def369810b64a79fe53e7b6319dba0b
Matthew <matthewjwright07@gmail.com>
2025-06-26 22:04:33 -0500

    got rid of animations

--- components/Wrappers/Wrapper.js
+++ components/Wrappers/Wrapper.js
@@ -1,18 +1,6 @@
-"use client"
-
-import { useEffect } from "react"
-import AOS from "aos"
-import "aos/dist/aos.css"
-
 export default function Wrapper({children}){
-    useEffect(() => {
-    AOS.init({
-        once: false,
-        mirror: true,
-    });
-    }, []);
     return (
-        <div className="px-8 lg:px-16 py-10" data-aos="zoom-in" data-aos-duration="600">
+        <div className="px-8 lg:px-16 py-10">
             {children}
         </div>
     )
