cb3bd6fe2e83485b557b835f637d988731929282
Matthew <matthewjwright07@gmail.com>
2025-08-28 14:36:58 -0600

    Updates to portfolio page

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -2,6 +2,6 @@ import Portfolio from "@/components/Other/Portfolio"
 
 export default function PortfolioPage(){
     return (
-    <Portfolio more="false" data={[{title:"Legalo",p:"A legal document generator site made using Next.js and Tailwind CSS",href:"https://thelegalo.com",img:"/legalo.png"},{title:"Wedding Website",p:"A wedding website for a couple made using Wix",href:"https://savannahandandrew.com",img:"/weddingwebsite.png"},{title:"Pinzo",p:"A location tracking website made using Bootstrap and Vercel",href:"https://thepinzo.com",img:"/pinzo.png"},{title:"Therapix",p:"A therapy tips website made using Next.js and Tailwind CSS",href:"https://mytherapix.com",img:"/therapix.png"},{title:"Utilly",p:"A bunch of free tools to anyone to use",href:"https://utilly.com",img:"/utilly.png"}]}/>
+    <Portfolio more="false" data={[{title:"Legalo",p:"A legal document generator site made using Next.js and Tailwind CSS",href:"https://thelegalo.com",img:"/legalo.png"},{title:"Wedding Website",p:"A wedding website for a couple made using Wix",href:"https://savannahandandrew.com",img:"/weddingwebsite.png"},{title:"Pinzo",p:"A location tracking website made using Bootstrap and Vercel",href:"https://thepinzo.com",img:"/pinzo.png"},{title:"Therapix",p:"A therapy tips website made using Next.js and Tailwind CSS",href:"https://mytherapix.com",img:"/therapix.png"},{title:"Utilly",p:"A bunch of free tools to anyone to use",href:"https://utilly.io",img:"/utilly.png"}]}/>
     )
 }
